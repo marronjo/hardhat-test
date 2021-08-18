1f14f00df6b232ebb68787b5a0f3c940918e63c4
Joe Marron <68810618+marronjo@users.noreply.github.com>
2021-08-18 23:43:01 +0100

    Ownership fxs + return Space info

--- hardhat/contracts/SpaceExample.sol
+++ hardhat/contracts/SpaceExample.sol
@@ -3,17 +3,18 @@ pragma solidity 0.7.3;
 
 contract SpaceExample{
 
-    enum Visibility{
-        onlyVisible,
+    //0 - 3 
+    enum Visibility{   
+        onlyVisible,    
         onlyHidden,
         onlyPublic,
         onlyUnlisted
-    }
+    }   
 
     struct Space {
         address owner;
         string handle;
-        string[] content_cids;
+        //string[] content_cids;    //array of content ids on ipfs
         uint block_num;
         uint timestamp;
         Visibility visibility;  //4 possible states, enum used to track
@@ -23,19 +24,13 @@ contract SpaceExample{
 
     mapping(uint => Space) public spaces;  //maps space id to space structure
     
-    address[] public array;
+    address[] public addresses;
 
     uint spaceIdIncriment = 0;    //initialise space id counter 
 
     function createSpace(string memory _handle, Visibility _visibility) public {
     
-        spaceIdIncriment = array.length;
-        
-        /*
-        space.owner = msg.sender;
-        space.handle = _handle;
-        space.visibility = _visibility;
-        */
+        spaceIdIncriment = addresses.length;
 
         spaces[spaceIdIncriment].owner = msg.sender;
         spaces[spaceIdIncriment].handle = _handle;
@@ -43,10 +38,52 @@ contract SpaceExample{
         spaces[spaceIdIncriment].timestamp = block.timestamp;
         spaces[spaceIdIncriment].visibility = _visibility;      // uint 0-3 represents 4 states of visibility
         spaces[spaceIdIncriment].followers_count = 0;
-        array.push(msg.sender);
+        addresses.push(msg.sender);
     }
 
-    function getOwner(uint spaceId) view public returns(address) {
-        return spaces[spaceId].owner;
+    function transferOwnership(address _newOwner, uint _spaceId) public {
+        require(msg.sender == spaces[_spaceId].owner);  //must be current owner to transfer ownership
+        spaces[_spaceId].owner = _newOwner;
+        addresses[_spaceId] = _newOwner;
+    }
+
+    function changeHandle(string memory _newHandle, uint _spaceId) public {
+        require(msg.sender == spaces[_spaceId].owner);  //must be current owner to change handle
+        spaces[_spaceId].handle = _newHandle;
+    }
+
+    function getOwner(uint _spaceId) view public returns(address) {
+        return spaces[_spaceId].owner;
+    }
+
+    function getHandle(uint _spaceId) view public returns(string memory) {
+        return spaces[_spaceId].handle;
+    }
+    
+    function getBlockNumber(uint _spaceId) view public returns(uint) {
+        return spaces[_spaceId].block_num;
+    }
+
+    function getTimestamp(uint _spaceId) view public returns(uint) {
+        return spaces[_spaceId].timestamp;
+    }
+
+    function getVisibility(uint _spaceId) view public returns(Visibility) {
+        return spaces[_spaceId].visibility;
+    }
+
+    function getFollowerCount(uint _spaceId) view public returns(uint) {
+        return spaces[_spaceId].followers_count;    //or return spaces[_spaceId].followers.length
+    }
+
+    function findSpaceById(uint _spaceId) view public returns(address, string memory, uint, uint, Visibility, uint){
+        return(
+            spaces[_spaceId].owner,
+            spaces[_spaceId].handle,
+            spaces[_spaceId].block_num,
+            spaces[_spaceId].timestamp,
+            spaces[_spaceId].visibility,
+            spaces[_spaceId].followers_count
+        );
     }
 }
